70c82ad16087e6bb214a18211a311f481beb7620
LeManuelo <lemanuelo04s@gmail.com>
2023-05-22 20:07:02 -0500

    Update Futbolista.java

--- src/main/java/futbol/Futbolista.java
+++ src/main/java/futbol/Futbolista.java
@@ -6,6 +6,7 @@ public abstract class Futbolista implements Comparable<Object>{
 	private final String posicion;
 	
 	public Futbolista(String nombre,int edad,String posicion) {
+		this.nombre=nombre;
 		this.edad=edad;
 		this.posicion=posicion;	
 	}
